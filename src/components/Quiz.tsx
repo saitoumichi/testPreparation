1d237ce3677678ca7cc5927cde0aecb3e432fafb
Michi Saito <ms20054869tv@gmail.com>
2025-07-26 13:33:50 +0900

    tanomu

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -196,7 +196,6 @@ export default function Quiz() {
             <div className="w-full bg-gray-200 rounded-full h-2">
               <div 
                 className="bg-blue-600 h-2 rounded-full transition-all duration-100"
-                style={{ width: `${progressPercent}%` }}
               ></div>
             </div>
           </div>
